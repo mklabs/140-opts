415f74e9da20cbc347024bc3534f49cf59f7d53a
Mickael Daniel <daniel.mickael@gmail.com>
2016-06-01 00:49:16 +0200

    test: updates, add .travis

--- test/index.js
+++ test/index.js
@@ -1,21 +1,36 @@
-var opts = require('../'),
-  assert = require('assert');
-
-// `node test` --> asserts
-
-// `node test --some thing` --> { some: 'thing' }
+var opts   = require('../');
+var assert = require('assert');
 
 var args = process.argv.slice(2);
-if(args.length) console.log(opts(args)); return;
+if(args.length) return console.log(opts(args));
 
-var o = opts(['--foo', 'foo', '--bar', 'bar', '-b', 'baz', '-o', 'woot', '--blah']);
+var args = ['--foo', 'foo', '--bar', 'bar', '-b', 'baz', '-o', 'woot', '--blah'];
+var argv = opts(args);
 
-console.log(o);
-
-assert.deepEqual(o, {
+assert.deepEqual(argv, {
   foo: 'foo',
   bar: 'bar',
   b: 'baz',
   o: 'woot',
   blah: true
 });
+
+console.log(`
+
+    Success!
+
+    $ ${args.join(' ')}
+
+    ${require('util').inspect(argv, { colors: true })}
+
+    \\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/
+    \\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/
+    \\o/\\o/\\o/\\o/\\o/                        \\o/\\o/\\o/\\o/\\o/\\o/\\o/
+    \\o/\\o/\\o/\\o/\\o/                        \\o/\\o/\\o/\\o/\\o/\\o/\\o/
+    \\o/\\o/\\o/\\o/\\o/       We did it!       \\o/\\o/\\o/\\o/\\o/\\o/\\o/
+    \\o/\\o/\\o/\\o/\\o/                        \\o/\\o/\\o/\\o/\\o/\\o/\\o/
+    \\o/\\o/\\o/\\o/\\o/                        \\o/\\o/\\o/\\o/\\o/\\o/\\o/
+    \\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/
+    \\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/
+    \\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/\\o/
+`);
