dc9429a21c665b8e595cd6034f8fe1c9c2dbcd0a
Mickael Daniel <daniel.mickael@gmail.com>
2016-06-01 00:16:01 +0200

    Expand minified opts function (242-opts)

--- opts.js
+++ opts.js
@@ -1,17 +1,15 @@
-module.exports =
+// module.exports = function(a){return a.map(function(c,d){return{n:c.match(/^--?(.+)/),v:a[d+1]||!0}}).reduce(function(a,b){b.n&&(a[b.n[1]]=b.v);return a},{})}
 
-   function(a){return a.map(function(c,d){return{n:c.match(/^--?(.+)/),v:a[d+1]||!0}}).reduce(function(a,b){b.n&&(a[b.n[1]]=b.v);return a},{})}
-
-// function(a){return a.map(function(b,c){return{n:b.match(/^--?(.+)/),v:a[c+1]||!0}}).reduce(function(a,b){if(b.n)a[b.n[1]]=b.v;return a},{})}
-
-// function(a){return a.map(function(c,d){return{n:c.match(/^--?(.+)/),v:a[d+1]||!0}}).reduce(function(a,b){b.n&&(a[b.n[1]]=b.v);return a},{})}
-
-// function(a){return a.map(function(c,d){return{n:c.match(/^--?(.+)/),v:a[d+1]||!!a}}).reduce(function(a,b){b.n&&(a[b.n[1]]=b.v);return a},{})}
-
-// function(a){return a.map(function(c,d){c=c.match(/^--?(.+)/);return c&&{n:c[1],v:a[d+1]||!!a}}).reduce(function(a,b){b&&(a[b.n]=b.v);return a},{})}
-
-// function(a){return a.map(function(c,d){c=c.match(/^--?([\w]+)/);return c&&{n:c[1],v:a[d+1]||!!a}}).reduce(function(a,b){b&&(a[b.n]=b.v);return a},{})}
-
-// function(a,l){l=/^--?([\w]+)/;return a.map(function(a,b,c,d){d=a.match(l);return d&&{n:d[1],v:c[b+1]||!!d}}).reduce(function(a,b){b&&(a[b.n]=b.v);return a;},{})}
-
-
+module.exports = function (a) {
+  return a
+    .map(function (c, d) {
+      return {
+        n: c.match(/^--?(.+)/),
+        v: a[d+1]||!0
+      };
+    })
+    .reduce(function (a, b) {
+      b.n&&(a[b.n[1]]=b.v);
+      return a;
+    }, {})
+};
