efa3c87a0a77919a2fb8e90c888adfc85be9e953
hemanth.hm <hemanth.hm@gmail.com>
2016-06-01 05:32:35 +0530

    159 chars ;)
    For node version >=4

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 // module.exports = function(a){return a.map(function(c,d){return{n:c.match(/^--?(.+)/),v:a[d+1]||!0}}).reduce(function(a,b){b.n&&(a[b.n[1]]=b.v);return a},{})}
 
-module.exports = function (a) {
-  return a
-    .map(function (c, d) {
-      return {
-        n: c.match(/^--?(.+)/),
-        v: a[d+1]||!0
-      };
-    })
-    .reduce(function (a, b) {
-      b.n&&(a[b.n[1]]=b.v);
-      return a;
-    }, {})
-};
+export default a => a
+  .map((c, d) => ({
+  n: c.match(/^--?(.+)/),
+  v: a[d+1]||!0
+}))
+  .reduce((a, b) => {
+    b.n&&(a[b.n[1]]=b.v);
+    return a;
+  }, {});
