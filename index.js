5848a0db25c0ee6d5be381ad3f216e50da2c4c6b
Mickael Daniel <daniel.mickael@gmail.com>
2016-06-05 05:11:47 +0200

    format

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // module.exports = function(a){return a.map(function(c,d){return{n:c.match(/^--?(.+)/),v:a[d+1]||!0}}).reduce(function(a,b){b.n&&(a[b.n[1]]=b.v);return a},{})}
 
 module.exports = a => a
-  .map((c, d) => ({
+
+.map((c, d) => ({
   n: c.match(/^--?(.+)/),
   v: a[d+1]||!0
 }))
-  .reduce((a, b) => {
-    b.n&&(a[b.n[1]]=b.v);
-    return a;
-  }, {});
+
+.reduce((a, b) => {
+  b.n&&(a[b.n[1]]=b.v);
+  return a;
+}, {});
